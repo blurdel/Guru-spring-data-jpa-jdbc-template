d8c3161e7d6c09a323887ff0cacd3d7fbc2e32ab
blurdel <5648737+blurdel@users.noreply.github.com>
2023-02-15 10:38:56 -0500

    misc updates

--- src/main/java/com/blurdel/sdjpajdbc/dao/AuthorDao.java
+++ src/main/java/com/blurdel/sdjpajdbc/dao/AuthorDao.java
@@ -4,7 +4,7 @@ import com.blurdel.sdjpajdbc.domain.Author;
 
 public interface AuthorDao {
 
-Author getById(Long id);
+	Author getById(Long id);
 	
 	Author getByName(String firstName, String lastName);
 	
